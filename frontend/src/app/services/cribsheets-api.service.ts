fe775e0123816b497839f8d56fabbf9f3363b546
llc <touldeguiacranmer@gmail.com>
2018-11-08 22:11:44 -0600

    auth

--- frontend/src/app/services/cribsheets-api.service.ts
+++ frontend/src/app/services/cribsheets-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { environment } from '../../environments/environment';
 import { Cribsheet } from '../../models/cribsheet';
